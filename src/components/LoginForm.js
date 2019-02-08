7623afe8a061e876104c33a628ec9ae611309a1a
Simon Clarke <simon@zeak.co>
2019-02-08 16:35:52 +0000

    Pouch/couch syncing

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,34 +1,92 @@
-import React from 'react'
-import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+import React from "react";
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Image,
+  Message,
+  Segment
+} from "semantic-ui-react";
 
-import styles from '../css/LoginForm.module.scss'
+import styles from "../css/LoginForm.module.scss";
 
-console.log(styles);
+import db, { startSync } from "../pouch.js";
 
-const LoginForm = () => (
-  <div className={ styles.LoginForm }>
-    <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
-      <Grid.Column style={{ maxWidth: 450 }}>
-        <Form size='large'>
-          <Segment stacked>
-            <Form.Input fluid icon='user' iconPosition='left' placeholder='Username' />
-            <Form.Input
-              fluid
-              icon='lock'
-              iconPosition='left'
-              placeholder='Password'
-              type='password'
-            />
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-            <Button color='teal' fluid size='large'>
-              Login
-            </Button>
-          </Segment>
-        </Form>
-      </Grid.Column>
-    </Grid>
-  </div>
-)
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
 
-export default LoginForm
+    this.setState({
+      [name]: value
+    });
+  }
 
+  doSubmit = () => {
+    startSync(this.state.username, this.state.password);
+  };
+
+  render() {
+    return (
+      <div className={styles.LoginForm}>
+        <Grid
+          textAlign="center"
+          style={{ height: "100%" }}
+          verticalAlign="middle"
+        >
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <Form size="large">
+              <Segment stacked>
+                <Form.Input
+                  fluid
+                  icon="user"
+                  iconPosition="left"
+                  placeholder="Username"
+                  name="username"
+                  onChange={e => this.handleInputChange(e)}
+                />
+                <Form.Input
+                  fluid
+                  icon="lock"
+                  iconPosition="left"
+                  placeholder="Password"
+                  name="password"
+                  type="password"
+                  onChange={e => this.handleInputChange(e)}
+                />
+
+                <Button
+                  color="teal"
+                  fluid
+                  size="large"
+                  onClick={() => this.doSubmit()}
+                >
+                  Login
+                </Button>
+
+                <Button
+                  color="teal"
+                  fluid
+                  size="large"
+                  onClick={() => {
+                    db.post({ ts: Math.random() });
+                  }}
+                >
+                  Add stuffs
+                </Button>
+              </Segment>
+            </Form>
+          </Grid.Column>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
